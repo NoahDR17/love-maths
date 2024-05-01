97c4a418fa4dd35949e379ea8fb15bf1325ad7a4
NoahDR17 <noahdavisreynolds@hotmail.com>
2024-05-01 16:34:25 +0100

    Feat: Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,26 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert('You clicked submit!')
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`)
+                runGame(gameType)
             }
         })
     }
+    runGame('addition')
 })
 /** 
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed.
  */
-function runGame(){
+function runGame(gameType){
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
+    if (gameType === 'addition'){
+        displayAdditionQuestion(num1, num2)
+    }  else {
+        alert(`unknown game type: ${gameType}`)
+        throw `unknown game type ${gameType}. Aborting!`;
+    }
 }
 function checkAnswer(){
 
@@ -32,8 +39,11 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAdditionQuestion(){
-    
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
+
 }
 function displaySubtractQuestion(){
 
