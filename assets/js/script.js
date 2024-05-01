80e8f5256d94314eaf38a8c6f8a123c1f2dc982f
NoahDR17 <noahdavisreynolds@hotmail.com>
2024-05-01 15:29:06 +0100

    Feat: set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+    
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+    
+}
